538eca667dcb5fa706127d1bc556c92a4bec2851
lin <550210817@qq.com>
2023-05-25 11:30:44 +0800

    添加EmptyCacheHook

--- config/fast_vqa/faster_vqa.py
+++ config/fast_vqa/faster_vqa.py
@@ -121,7 +121,7 @@ visualizer = dict(
 
 default_hooks = dict(
     checkpoint=dict(type='CheckpointHook', interval=1))
-custom_hooks = [dict(type='EMAHook',momentum=0.999)]
+custom_hooks = [dict(type='EMAHook',momentum=0.999),dict(type='EmptyCacheHook', after_epoch=True)]
 launcher = 'none'
 env_cfg = dict(
     cudnn_benchmark=False,
