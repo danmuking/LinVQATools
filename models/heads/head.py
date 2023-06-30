fe4fe7e71f7559e76c5664e132dc5b6a7b624c1c
lin <550210817@qq.com>
2023-06-30 10:14:31 +0800

    添加dover model实现

--- models/heads/head.py
+++ models/heads/head.py
@@ -7,15 +7,17 @@ class VQAHead(nn.Module):
         in_channels: input channels for MLP
         hidden_channels: hidden channels for MLP
         dropout_ratio: the dropout ratio for features before the MLP (default 0.5)
+        pre_pool: whether pre-pool the features or not (True for Aesthetic Attributes, False for Technical Attributes)
     """
 
     def __init__(
-            self, in_channels=768, hidden_channels=64, dropout_ratio=0.5, **kwargs
+            self, in_channels=768, hidden_channels=64, dropout_ratio=0.5, pre_pool=False, **kwargs
     ):
         super().__init__()
         self.dropout_ratio = dropout_ratio
         self.in_channels = in_channels
         self.hidden_channels = hidden_channels
+        self.pre_pool = pre_pool
         if self.dropout_ratio != 0:
             self.dropout = nn.Dropout(p=self.dropout_ratio)
         else:
@@ -25,11 +27,10 @@ class VQAHead(nn.Module):
         self.gelu = nn.GELU()
 
         self.avg_pool = nn.AdaptiveAvgPool3d((1, 1, 1))
-        self.fc = nn.Linear(16*7*7,1)
 
     def forward(self, x, rois=None):
+        if self.pre_pool:
+            x = self.avg_pool(x)
         x = self.dropout(x)
         qlt_score = self.fc_last(self.dropout(self.gelu(self.fc_hid(x))))
-        # qlt_score = self.gelu(qlt_score).reshape(qlt_score.shape[0],-1)
-        # qlt_score = self.fc(qlt_score)
         return qlt_score
---
+++ test/test_dover.py
@@ -0,0 +1,20 @@
+import os
+from unittest import TestCase
+
+import torch
+
+from models.dover import DOVER
+
+
+class TestDOVER(TestCase):
+    def testDover(self):
+        os.chdir('../')
+        args = {'backbone': {'technical': {'type': 'swin_tiny_grpb', 'checkpoint': True, 'pretrained': None},
+                             'aesthetic': {'type': 'conv_tiny'}}, 'backbone_preserve_keys': 'technical,aesthetic',
+                'divide_head': True, 'vqa_head': {'in_channels': 768, 'hidden_channels': 64}}
+        evaluator = DOVER(**args)
+        views = dict()
+        views['technical'] = torch.zeros((1, 3, 32, 224, 224))
+        views['aesthetic'] = torch.zeros((1, 3, 32, 224, 224))
+        result = evaluator(views)
+        print(result)
