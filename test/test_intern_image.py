c941d86ebc2f522422c0383d084eaa6b168702cd
lin <550210817@qq.com>
2023-07-25 10:49:09 +0800

    添加backbone完成

---
+++ test/test_intern_image.py
@@ -0,0 +1,39 @@
+import os
+from unittest import TestCase
+
+import torch
+
+from models.backbones.intern_image.build import build_model
+from models.backbones.intern_image.intern_image import InternImage
+
+
+class TestInternImage(TestCase):
+    def test_intern_image_backbone(self):
+        os.chdir('../')
+        config = dict(
+            model=dict(
+                type='intern_image',
+                drop_path_rate=0.1,
+                intern_image=dict(
+                    core_op='DCNv3',
+                    depths=[4, 4, 18, 4],
+                    groups=[4, 8, 16, 32],
+                    channels=64,
+                    offset_scale=1.0,
+                    mlp_ratio=4.0
+                )
+            )
+        )
+        model = InternImage(
+            core_op='DCNv3',
+            num_classes=1,
+            channels=64,
+            depths=[4, 4, 18, 4],
+            groups=[4, 8, 16, 32],
+            offset_scale=1.0,
+            mlp_ratio=4.0,
+        ).cuda()
+        print(model)
+        x = torch.zeros((2,3,224,224)).cuda()
+        ans = model(x)
+        print(ans)
