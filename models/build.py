ec22f87626ffcceb79b7d26514ac58e9121d3cf4
lin <550210817@qq.com>
2023-08-01 10:48:09 +0800

    feat: 修改bifomer卷积大小,删除biformer骨干网络最后的平均层

--- models/build.py
+++ models/build.py
@@ -3,8 +3,8 @@ from typing import Dict
 from models.faster_vqa import FasterVQA
 
 
-def build(config:Dict):
-    if config['model_type']=='faster_vqa':
+def build(config: Dict):
+    if config['model_type'] == 'faster_vqa':
         model = FasterVQA(
 
         )
