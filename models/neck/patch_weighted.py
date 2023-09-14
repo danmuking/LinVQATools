e8e0e19c8d81ccaa665d720616e242b9dde5099e
lin <550210817@qq.com>
2023-09-14 10:16:12 +0800

    09141015 feat: swin patch weight 增加norm

--- models/neck/patch_weighted.py
+++ models/neck/patch_weighted.py
@@ -8,18 +8,22 @@ class PatchWeighted(nn.Module):
         super().__init__()
         self.batch1 = nn.Sequential(
             Conv3d(768, 128, kernel_size=1),
+            nn.LayerNorm([128, 8, 7, 7]),
             nn.GELU(),
             Conv3d(128, 1, kernel_size=1),
         )
         self.batch2 = nn.Sequential(
             Conv3d(768, 128, kernel_size=1),
+            nn.LayerNorm([128, 8, 7, 7]),
             nn.GELU(),
             Conv3d(128, 1, kernel_size=1),
         )
+        self.norm = nn.LayerNorm([1, 8, 7, 7])
 
     def forward(self, x):
         x = x[0][0]
         # print(x.shape)
         score = self.batch1(x)
         weight = self.batch2(x)
-        return [[torch.mul(score, weight)]]
+        score = torch.mul(score, weight)
+        return [[self.norm(score)]]
