4f39b8bbf1467e23d9e950fb0b45f6b5cd94c88c
lin <550210817@qq.com>
2023-09-15 10:17:16 +0800

    09151016 feat: swin patch weight 分别增加一个swin块

--- models/neck/patch_weighted.py
+++ models/neck/patch_weighted.py
@@ -3,27 +3,65 @@ from einops import rearrange
 from torch import nn
 from torch.nn import Conv3d
 
+from models.backbones.swin_backbone import BasicLayer
+
 
 class PatchWeighted(nn.Module):
     def __init__(self):
         super().__init__()
         self.dropout = nn.Dropout(p=0.5)
         self.batch1 = nn.ModuleList([
+            BasicLayer(
+                dim=768,
+                depth=2,
+                num_heads=24,
+                window_size=(8, 7, 7),
+                mlp_ratio=4.0,
+                qkv_bias=True,
+                qk_scale=None,
+                drop=0.0,
+                attn_drop=0.0,
+                # drop_path=dpr[sum(depths[:i_layer]): sum(depths[: i_layer + 1])],
+                drop_path=[0.09090909361839294, 0.10000000149011612],
+                norm_layer=nn.LayerNorm,
+                downsample=None,
+                use_checkpoint=True,
+                jump_attention=False,
+                frag_bias=False,
+            ),
+            nn.LayerNorm(768),
             self.dropout,
-            Conv3d(768, 128, kernel_size=1),
-            # nn.LayerNorm(128),
+            Conv3d(768, 64, kernel_size=1),
             nn.GELU(),
             self.dropout,
-            Conv3d(128, 1, kernel_size=1),
+            Conv3d(64, 1, kernel_size=1),
             nn.GELU(),
         ])
         self.batch2 = nn.ModuleList([
+            BasicLayer(
+                dim=768,
+                depth=2,
+                num_heads=24,
+                window_size=(8, 7, 7),
+                mlp_ratio=4.0,
+                qkv_bias=True,
+                qk_scale=None,
+                drop=0.0,
+                attn_drop=0.0,
+                # drop_path=dpr[sum(depths[:i_layer]): sum(depths[: i_layer + 1])],
+                drop_path=[0.09090909361839294, 0.10000000149011612],
+                norm_layer=nn.LayerNorm,
+                downsample=None,
+                use_checkpoint=True,
+                jump_attention=False,
+                frag_bias=False,
+            ),
+            nn.LayerNorm(768),
             self.dropout,
-            Conv3d(768, 128, kernel_size=1),
-            # nn.LayerNorm(128),
+            Conv3d(768, 64, kernel_size=1),
             nn.GELU(),
             self.dropout,
-            Conv3d(128, 1, kernel_size=1),
+            Conv3d(64, 1, kernel_size=1),
             nn.GELU(),
         ])
 
@@ -34,18 +72,18 @@ class PatchWeighted(nn.Module):
         for m in self.batch1:
             if isinstance(m, nn.LayerNorm):
                 b, c, t, h, w = score.shape
-                score = rearrange(score, "b c t h w->b (t h w) c")
+                score = rearrange(score, "n c d h w -> n d h w c")
                 score = m(score)
-                score = rearrange(score, "b (t h w) c->b c t h w", t=t, h=h, w=w)
+                score = rearrange(score, "n d h w c -> n c d h w")
             else:
                 score = m(score)
         weight = x
         for m in self.batch2:
             if isinstance(m, nn.LayerNorm):
                 b, c, t, h, w = weight.shape
-                weight = rearrange(weight, "b c t h w->b (t h w) c")
+                weight = rearrange(weight, "n c d h w -> n d h w c")
                 weight = m(weight)
-                weight = rearrange(weight, "b (t h w) c->b c t h w", t=t, h=h, w=w)
+                weight = rearrange(weight, "n d h w c -> n c d h w")
             else:
                 weight = m(weight)
         score = torch.mul(score, weight)
