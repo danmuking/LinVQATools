b9214fb60ee46652ca6541a291b6600bee3dbded
lin <550210817@qq.com>
2023-09-27 10:21:26 +0800

    feat: patch weight 多卡

--- config/fast_vqa/faster_vqa_ema.py
+++ config/fast_vqa/faster_vqa_ema.py
@@ -22,7 +22,7 @@ model = dict(
 )
 epochs = 600
 batch_size = 6
-num_workers = 12
+num_workers = 4
 prefix = 'temp/fragment'
 argument = [
     dict(
@@ -132,11 +132,11 @@ val_evaluator = [
 default_hooks = dict(
     checkpoint=dict(type='CheckpointHook', interval=1, max_keep_ckpts=10, save_best='SROCC', rule='greater'))
 custom_hooks = [
-    dict(type='TrainEvaluatorHook'),
+    # dict(type='TrainEvaluatorHook'),
     # dict(type='CustomEMAHook',momentum=0.01)
     # dict(type='EmptyCacheHook', after_epoch=True)
 ]
-launcher = 'none'
+launcher = 'pytorch'
 randomness = dict(seed=42)
 # randomness = dict(seed=3407)
 # randomness = dict(seed=114514)
