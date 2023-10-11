aefb2afca48382fef1c7ad0e485ca90ed0f5ad52
lin <550210817@qq.com>
2023-10-11 12:40:39 +0800

    10111239 vit patch16 fragment32

--- config/fast_vqa/vit.py
+++ config/fast_vqa/vit.py
@@ -2,13 +2,13 @@ custom_imports = dict(
     imports=['faster_vqa', 'default_dataset', 'srocc', 'rmse',
              'plcc', 'krcc', 'train_evaluator_hook', 'custom_ema_hook'],
     allow_failed_imports=False)
-work_dir = 'work_dir/faster_vqa/10110840 vit_patch16_fragment32'
+work_dir = 'work_dir/faster_vqa/10111239 vit_patch16_fragment32'
 visualizer = dict(
     type='Visualizer',
     vis_backends=[
         dict(
             type='WandbVisBackend',
-            init_kwargs=dict(project='faster vqa消融', name='10110840 vit patch16 fragment32')
+            init_kwargs=dict(project='faster vqa消融', name='10111239 vit patch16 fragment32')
         ),
     ],
 )
