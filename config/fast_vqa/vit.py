d67847c74a2ce8f34b6d2ff188e9ab6eb0ee47ba
lin <550210817@qq.com>
2023-10-15 17:44:49 +0800

    10151744 vit_patch16_fragment32

--- config/fast_vqa/vit.py
+++ config/fast_vqa/vit.py
@@ -2,13 +2,13 @@ custom_imports = dict(
     imports=['faster_vqa', 'default_dataset', 'srocc', 'rmse',
              'plcc', 'krcc', 'train_evaluator_hook', 'custom_ema_hook'],
     allow_failed_imports=False)
-work_dir = 'work_dir/faster_vqa/10151146 vit_patch16_fragment32'
+work_dir = 'work_dir/faster_vqa/10151744 vit_patch16_fragment32'
 visualizer = dict(
     type='Visualizer',
     vis_backends=[
         dict(
             type='WandbVisBackend',
-            init_kwargs=dict(project='faster vqa消融', name='10151146 vit patch16 fragment32')
+            init_kwargs=dict(project='faster vqa消融', name='10151744 vit patch16 fragment32')
         ),
     ],
 )
@@ -24,7 +24,7 @@ model = dict(
 epochs = 600
 batch_size = 4
 num_workers = 5
-prefix = 'fragment'
+prefix = 'temp'
 argument = [
         dict(
             name='FragmentShuffler',
