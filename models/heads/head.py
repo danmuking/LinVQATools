838601f3b5d5e525b05d791039e9a3a1e1d87004
lin <550210817@qq.com>
2023-10-18 14:05:25 +0800

    10181404 vit_patch16_fragment32 4frame atte

--- models/heads/head.py
+++ models/heads/head.py
@@ -24,35 +24,38 @@ class VQAHead(nn.Module):
         self.dropout_ratio = dropout_ratio
         self.in_channels = in_channels
         self.hidden_channels = hidden_channels
-        if self.dropout_ratio != 0:
-            self.dropout = nn.Dropout(p=self.dropout_ratio)
-        else:
-            self.dropout = None
-        self.fc_hid = nn.Conv3d(self.in_channels, self.hidden_channels, (1, 1, 1))
-        self.fc_last = nn.Conv3d(self.hidden_channels, 1, (1, 1, 1))
-        self.gelu = nn.GELU()
-        self.fc = nn.Linear(fc_in, 1)
+        self.fc_hid = nn.Sequential(
+            nn.Dropout(p=self.dropout_ratio) if self.dropout_ratio > 0 else nn.Identity(),
+            nn.Conv3d(self.in_channels, self.hidden_channels, (1, 1, 1)),
+            nn.GELU()
+        )
+        self.fc_last = nn.Sequential(
+            nn.Dropout(p=self.dropout_ratio) if self.dropout_ratio > 0 else nn.Identity(),
+            nn.Conv3d(self.hidden_channels, 1, (1, 1, 1)),
+            nn.GELU()
+        )
+        self.fc = nn.Sequential(
+            nn.Dropout(p=self.dropout_ratio) if self.dropout_ratio > 0 else nn.Identity(),
+            nn.Linear(fc_in, 1)
+        )
 
     def forward(self, x):
         x = x[0][0]
         logger.debug("head层输入维度: {}".format(x.shape))
         x = self.atte(x)
-        x = self.dropout(x)
         qlt_score = self.fc_hid(x)
         logger.debug('head: channel {}->{}'.format(x.shape[1],qlt_score.shape[1]))
-        qlt_score = self.gelu(qlt_score)
         channel_in = qlt_score.shape[1]
-        qlt_score = self.fc_last(self.dropout(qlt_score))
+        qlt_score = self.fc_last(qlt_score)
         channel_out = qlt_score.shape[1]
         logger.debug('head: channel {}->{}'.format(channel_in,channel_out))
 
         channel_in = qlt_score.shape
-        qlt_score = self.gelu(qlt_score).reshape(qlt_score.shape[0], -1)
+        qlt_score = qlt_score.reshape(qlt_score.shape[0], -1)
         channel_out = qlt_score.shape
         logger.debug('head: 展开 {}->{}'.format(channel_in, channel_out))
 
         channel_in = qlt_score.shape[1]
-        qlt_score = self.dropout(qlt_score)
         qlt_score = self.fc(qlt_score)
         channel_out = qlt_score.shape[1]
         logger.debug('head: Liner {}->{}'.format(channel_in, channel_out))
