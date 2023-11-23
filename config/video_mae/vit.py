f8267c3b286e4adfb338042e354df5100b171e3e
lin <550210817@qq.com>
2023-11-24 00:36:39 +0800

    11240034 vit random_cell_mask_75 mae last3

--- config/video_mae/vit.py
+++ config/video_mae/vit.py
@@ -2,7 +2,7 @@ custom_imports = dict(
     imports=['video_mae_vqa', 'default_dataset', 'srocc', 'rmse',
              'plcc', 'krcc', 'train_evaluator_hook', 'custom_ema_hook'],
     allow_failed_imports=False)
-name = "11231421 vit random_cell_mask_75 mae"
+name = "11240034 vit random_cell_mask_75 mae last3"
 work_dir = 'work_dir/video_mae_vqa/'+name
 visualizer = dict(
     type='Visualizer',
--- models/heads/vqa_mlp_head.py
+++ models/heads/vqa_mlp_head.py
@@ -260,20 +260,22 @@ class VQAPoolMlpHead(nn.Module):
         self.dropout_ratio = dropout_ratio
         self.in_channels = in_channels
         self.hidden_channels = hidden_channels
+        dim = 384*3
         self.encode_to_vqa = nn.Sequential(
-            nn.Linear(4608, 2 * 4608),
-            nn.Linear(2 * 4608, 4608),
+            nn.Linear(dim, 2 * dim),
+            nn.Linear(2 * dim, dim),
         )
         self.fc_hid = nn.Sequential(
             nn.Dropout(p=self.dropout_ratio) if self.dropout_ratio > 0 else nn.Identity(),
-            nn.Linear(4608, 4608 // 4),
-            nn.LayerNorm(4608 // 4, eps=1e-6)
+            nn.Linear(dim, dim // 4),
+            nn.LayerNorm(dim // 4, eps=1e-6)
         )
         self.fc_last = nn.Sequential(
-            nn.Linear(4608 // 4, 1),
+            nn.Linear(dim // 4, 1),
         )
 
     def forward(self, x):
+        x = x[-3:]
         for i in range(len(x)):
             x[i] = self.norm(torch.cat([torch.mean(x[i], dim=1)], dim=-1))
         x = torch.cat(x, dim=-1)
