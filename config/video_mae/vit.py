59e71fece2c8c89ca78a3e3468aea8615c3497ff
lin <550210817@qq.com>
2023-12-01 14:57:41 +0800

    12011456 vit random_cell_mask_75 mae last6 4clip

--- config/video_mae/vit.py
+++ config/video_mae/vit.py
@@ -2,7 +2,7 @@ custom_imports = dict(
     imports=['video_mae_vqa', 'default_dataset', 'srocc', 'rmse',
              'plcc', 'krcc', 'train_evaluator_hook', 'custom_ema_hook'],
     allow_failed_imports=False)
-name = "12011454 vit random_cell_mask_75 mae last6 4clip"
+name = "12011456 vit random_cell_mask_75 mae last6 4clip"
 work_dir = 'work_dir/video_mae_vqa/'+name
 visualizer = dict(
     type='Visualizer',
@@ -147,7 +147,7 @@ launcher = 'none'
 # randomness = dict(seed=42)
 # randomness = dict(seed=3407)
 # randomness = dict(seed=114514)
-randomness = dict(seed=123123)
+randomness = dict(seed=237376)
 env_cfg = dict(
     cudnn_benchmark=True,
     backend='nccl',
