42fa6e2fa27eafe8ed8dac5b0f90636ab2a75e4c
lin <550210817@qq.com>
2024-09-09 15:48:54 +0800

    09091545 model clip resize img baseline 4clip

--- config/model/model.py
+++ config/model/model.py
@@ -2,7 +2,7 @@ custom_imports = dict(
     imports=['model', 'default_dataset', 'srocc', 'rmse',
              'plcc', 'krcc', 'train_evaluator_hook', 'custom_ema_hook'],
     allow_failed_imports=False)
-name = "05281411 model clip resize img baseline 4clip"
+name = "09091545 model clip resize img baseline 4clip"
 work_dir = 'work_dir/model/' + name
 visualizer = dict(
     type='Visualizer',
