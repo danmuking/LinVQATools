0dbf586316acf47eb27b0d5047edf829b8bd5d6b
lin <550210817@qq.com>
2025-01-03 09:25:17 +0800

    01030918 lmodel resume

--- config/lmodel/lmodel.py
+++ config/lmodel/lmodel.py
@@ -2,7 +2,7 @@ custom_imports = dict(
     imports=['last_model', 'default_dataset', 'srocc', 'rmse',
              'plcc', 'krcc', 'train_evaluator_hook', 'custom_ema_hook'],
     allow_failed_imports=False)
-name = "12311616 lmodel resume"
+name = "01030918 lmodel resume"
 work_dir = 'work_dir/lmodel/'+name
 visualizer = dict(
     type='Visualizer',
@@ -16,7 +16,7 @@ visualizer = dict(
 model = dict(
     type='LModelWrapper',
     model_type='s',
-    mask_ratio=0.75,
+    mask_ratio=0.25,
     head_dropout=0.1,
     drop_path_rate=0.1
 )
