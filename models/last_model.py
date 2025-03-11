ad552f0f5b27c07ef847a979db712b4c5825215a
lin <550210817@qq.com>
2025-03-11 16:57:29 +0800

    03111104 lmodel

--- models/last_model.py
+++ models/last_model.py
@@ -164,8 +164,8 @@ class LModel(nn.Module):
 
     #     resnet
     #     device = "cuda"
-        pretrained_cfg_overlay = {'file' : r"/data/ly/code/LinVQATools/pretrained_weights/resnet50clip/pytorch_model.bin"}
-        self.spatio_model = timm.create_model('resnet50_clip', pretrained=True,
+        pretrained_cfg_overlay = {'file' : r"/data/ly/code/LinVQATools/pretrained_weights/efficientnet/pytorch_model.bin"}
+        self.spatio_model = timm.create_model('efficientnet_b0', pretrained=True,
                                               pretrained_cfg_overlay=pretrained_cfg_overlay,
                                               num_classes=0,
                                               )
@@ -173,7 +173,7 @@ class LModel(nn.Module):
 
     #     fusion part
         self.fusion_module = Fusion()
-        self.linear1 = nn.Linear(2048,1)
+        self.linear1 = nn.Linear(1280,1)
         self.fusion = nn.Linear(2,1)
         self.score = nn.Linear(256,1)
 
