2f88502ebd70ac1f4ca1e7b6f0323676eb01b25f
lin <550210817@qq.com>
2025-03-14 21:10:32 +0800

    03142108 lmodel

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 # os.environ["CUDA_VISIBLE_DEVICES"] = ''
 os.environ["WANDB_API_KEY"] = '2125be99aae223011562a86318a7e1edd8556487'
@@ -9,6 +10,8 @@ import init_environment
 from mmengine import Config
 
 from mmengine.runner import Runner
+
+time.sleep(3600*5)
 
 config = Config.fromfile('./config/lmodel/lmodel.py')
 runner = Runner.from_cfg(config)
