3e29f3d720eb27adea8f0e70dbf2d09e0c5ab621
lin <550210817@qq.com>
2025-03-14 08:30:30 +0800

    03132113 lmodel

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 # os.environ["CUDA_VISIBLE_DEVICES"] = ''
 os.environ["WANDB_API_KEY"] = '2125be99aae223011562a86318a7e1edd8556487'
@@ -9,6 +10,9 @@ import init_environment
 from mmengine import Config
 
 from mmengine.runner import Runner
+
+# 等待5小时
+time.sleep(6 * 3600)
 
 config = Config.fromfile('./config/lmodel/lmodel.py')
 runner = Runner.from_cfg(config)
--- models/last_model.py
+++ models/last_model.py
@@ -3,13 +3,11 @@ from functools import partial
 from typing import Union, Dict, Optional
 
 import open_clip
-import timm
 import torch
 from einops import rearrange
 from mmengine import MODELS
 from mmengine.model import BaseModel
 from mmengine.optim import OptimWrapper
-from models.backbones.clip.model import AttentionPool2d
 from torch import nn
 
 from models.backbones.video_mae_v2 import VisionTransformer
@@ -40,24 +38,16 @@ class Fusion(nn.Module):
 
         self.linear1 = nn.Linear(384, 256)
         self.linear2 = nn.Linear(1176, 1024)
-        self.linear3 = nn.Linear(20 * 20, 256)
+        self.linear3 = nn.Linear(20*20, 256)
         self.linear4 = nn.Linear(1024, 1024)
         self.relu = nn.ReLU()
-        # x3
-        self.linear5 = nn.Linear(7 * 7, 256)
-        self.linear6 = nn.Linear(1024, 1024)
 
-        self.mhsa1 = MultiHeadAttention(1024, 1024, 1024, 6)
-        self.mhsa2 = MultiHeadAttention(1024, 1024, 1024, 6)
-        self.mhsa3 = MultiHeadAttention(1024, 1024, 1024, 6)
-        self.mhca1 = CrossAttention(1024, 1024, 1024, 1024, 6)
-        self.mhca2 = CrossAttention(1024, 1024, 1024, 1024, 6)
-        self.mhca3 = CrossAttention(1024, 1024, 1024, 1024, 6)
-        self.mhca4 = CrossAttention(1024, 1024, 1024, 1024, 6)
-        self.mhca5 = CrossAttention(1024, 1024, 1024, 1024, 6)
-        self.mhca6 = CrossAttention(1024, 1024, 1024, 1024, 6)
+        self.video_self_attn = MultiHeadAttention(1024, 1024, 1024, 6)
+        self.img_self_attn = MultiHeadAttention(1024, 1024, 1024, 6)
+        self.video_cross_attn = CrossAttention(1024, 1024, 1024, 1024, 6)
+        self.img_cross_attn = CrossAttention(1024, 1024, 1024, 1024, 6)
 
-    def forward(self, x1, x2, x3):
+    def forward(self, x1,x2):
         x1 = self.linear1(x1)
         x1 = self.relu(x1)
         x1 = rearrange(x1, 'b n c -> b c n')
@@ -65,37 +55,23 @@ class Fusion(nn.Module):
         # 256,1024
         x1 = self.relu(x1)
 
-        x2 = rearrange(x2, 'b n c -> b c n')
+        x2 = rearrange(x2, 'b c h w -> b c (h w)')
         x2 = self.linear3(x2)
         x2 = self.relu(x2)
-        x2 = rearrange(x2, 'b c n -> b n c')
+        x2 = rearrange(x2, 'b n c -> b c n')
         x2 = self.linear4(x2)
         # 256,1024
         x2 = self.relu(x2)
 
-        x3 = rearrange(x3, 'b n c -> b c n')
-        x3 = self.linear5(x3)
-        x3 = self.relu(x3)
-        x3 = rearrange(x3, 'b c n -> b n c')
-        x3 = self.linear6(x3)
-        # 256,1024
-        x3 = self.relu(x3)
-
         x1 = x1 / x1.norm(dim=2, keepdim=True)
         x2 = x2 / x2.norm(dim=2, keepdim=True)
-        x3 = x3 / x3.norm(dim=2, keepdim=True)
-        x1 = self.mhsa1(x1)
-        x2 = self.mhsa2(x2)
-        x3 = self.mhsa3(x3)
+        x1 = self.video_self_attn(x1)
+        x2 = self.img_self_attn(x2)
+        cross_video_feats = self.video_cross_attn(x2, x1)
+        cross_img_feats = self.img_cross_attn(x1, x2)
 
-        feat = self.mhca1(x1, x2)
-        feat = feat + self.mhca1(x2, x1)
-        feat = feat + self.mhca1(x1, x3)
-        feat = feat + self.mhca1(x3, x1)
-        feat = feat + self.mhca1(x2, x3)
-        feat = feat + self.mhca1(x3, x2)
 
-        return feat
+        return cross_video_feats + cross_img_feats
 
 
 class LModel(nn.Module):
@@ -177,19 +153,18 @@ class LModel(nn.Module):
         self.conv9 = nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=3, stride=1, padding=1)
         self.bn9 = nn.BatchNorm2d(1024)
         self.relu = nn.ReLU()
-        self.mhsa = MultiHeadAttention(1024, 1024, 1024, 6)
         self.maxpool = nn.MaxPool2d(1, stride=2)
 
-        #     resnet
-        #     device = "cuda"
-        self.resnet, _, _ = open_clip.create_model_and_transforms('RN50', pretrained='openai')
-        self.resnet.visual.attnpool = AttentionPool2d(7, 2048, 32, 1024)
+    #     resnet
+    #     device = "cuda"
+        self.resnet, _, preprocess = open_clip.create_model_and_transforms("RN50", pretrained="openai")
+        # self.clip_model = self.clip_model.to(device)
 
         #     fusion part
         self.fusion_module = Fusion()
-        self.linear1 = nn.Linear(1024, 1)
-        self.fusion = nn.Linear(2, 1)
-        self.score = nn.Linear(256, 1)
+        self.linear1 = nn.Linear(1024,1)
+        self.fusion = nn.Linear(2,1)
+        self.score = nn.Linear(256,1)
 
     def forward(self, inputs, mask):
 
@@ -301,22 +276,17 @@ class LModel(nn.Module):
         out = self.conv9(out)
         out = self.bn9(out)
         out = self.relu(out)
-        out = rearrange(out, 'b c h w -> b (h w) c')
-        out = self.mhsa(out)
-        out = self.relu(out)
         time_feat = out
 
         # resnet
         image_latent = self.resnet.visual(inputs['spa_img'])
-        image_latent = image_latent.permute(1, 0, 2)
-        image_latent = image_latent / image_latent.norm(dim=-1, keepdim=True)
-        spatio_feature = image_latent[:, 1:, ]
+        score3 = self.linear1(image_latent)
 
         # fusion part
-        fusion_feat = self.fusion_module(content_feat, time_feat, spatio_feature)
+        fusion_feat = self.fusion_module(content_feat,time_feat)
         fusion_feat = torch.mean(fusion_feat, dim=2)
         preds_score = self.score(fusion_feat)
-        preds_score = preds_score
+        preds_score = preds_score+score3
 
         output = {"preds_pixel": pred_pixels, "labels_pixel": labels, "preds_score": preds_score}
         return output
